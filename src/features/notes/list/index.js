08e47f4a4e4f444b019f1e1a0d684c0ce5384a3d
Eduardo <eduardo.tornquist@lennar.com>
2024-02-15 22:14:31 -0500

    Using useSelector() hook to select all the notes.

--- src/features/notes/list/index.js
+++ src/features/notes/list/index.js
@@ -1,5 +1,8 @@
+import { useSelector } from 'react-redux'
+import { selectAllNotes } from '../notes.slice'
+
 export default function NotesList() {
-  const notes = [];
+  const notes = useSelector(selectAllNotes)
 
   function handleDeleteButtonClick(id) {}
 
@@ -8,11 +11,11 @@ export default function NotesList() {
       <span>{note}</span>
       <button onClick={() => handleDeleteButtonClick(id)}>Delete</button>
     </li>
-  ));
+  ))
 
   return notesListItems.length > 0 ? (
     <ul>{notesListItems}</ul>
   ) : (
     <h3>Please add some notes</h3>
-  );
+  )
 }
